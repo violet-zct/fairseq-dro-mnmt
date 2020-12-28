1a8d358e052e0f2f6fd7aa34c0d11eef120c1150
Chunting Zhou <chuntinz@fb.com>
2020-12-28 03:06:41 -0800

    add small t

--- fairseq/models/transformer.py
+++ fairseq/models/transformer.py
@@ -956,6 +956,19 @@ def transformer_iwslt_de_en(args):
     base_architecture(args)
 
 
+@register_model_architecture("transformer", "transformer_small")
+def transformer_iwslt_de_en(args):
+    args.encoder_embed_dim = getattr(args, "encoder_embed_dim", 512)
+    args.encoder_ffn_embed_dim = getattr(args, "encoder_ffn_embed_dim", 1024)
+    args.encoder_attention_heads = getattr(args, "encoder_attention_heads", 4)
+    args.encoder_layers = getattr(args, "encoder_layers", 3)
+    args.decoder_embed_dim = getattr(args, "decoder_embed_dim", 512)
+    args.decoder_ffn_embed_dim = getattr(args, "decoder_ffn_embed_dim", 1024)
+    args.decoder_attention_heads = getattr(args, "decoder_attention_heads", 4)
+    args.decoder_layers = getattr(args, "decoder_layers", 3)
+    base_architecture(args)
+
+
 @register_model_architecture("transformer", "transformer_wmt_en_de")
 def transformer_wmt_en_de(args):
     base_architecture(args)
