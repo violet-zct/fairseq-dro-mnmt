9a0fa77ab355975e666c9e98cebb97ed25d09c94
Chunting Zhou <chuntinz@fb.com>
2020-12-30 11:19:05 -0800

    fix

--- v1_exps/send.sh
+++ v1_exps/send.sh
@@ -3,13 +3,14 @@ dirname=$1 # exp name
 end=$((SECONDS+114400))
 saved_models=saved_models/${dirname}
 send_dir=/home/chuntinz/tir5/logs/${dirname}
+end_file=$saved_models/END
 ssh tir "mkdir ${send_dir}"
 
 scp $saved_models/run.sh tir:${send_dir}/
 echo ${send_dir}
 while [ $SECONDS -lt $end ]; do
     scp $saved_models/log.txt tir:${send_dir}/
-    if [[ -f "$saved_models/END" ]]; then
+    if [[ -f ${end_file} ]]; then
         break
     fi
     sleep 1800s
