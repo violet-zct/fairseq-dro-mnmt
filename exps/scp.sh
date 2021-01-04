19588bf366fccc16ccbe96252d1343647319752b
Chunting Zhou <chuntinz@fb.com>
2021-01-04 12:13:09 -0800

    update

--- exps/scp.sh
+++ exps/scp.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-dir="1_analyze_ema0.1_alpha0.5_wu_ub_lang_dro_ted8 2_analyze_hier_ema0.1_alpha0.5_beta0.5_wu_ub_ted8 3_sanity_hier_ema0.1_alpha0.5_beta1.0_wu_ub_ted8"
+dir="4_baseline_ema0.1_alpha0.5_wu_ub_lang_dro_ted8 5_hier_baseline_ema0.1_alpha0.5_beta0.5_wu_ub_lang_dro_ted8 6_erm_ted8 7_outer_ub_hier_ema0.1_alpha0.5_beta0.5_wu_lang_dro_ted8 8_lr1e-4_ema0.1_alpha0.5_wu_ub_lang_dro_ted8 9_lr4e-4_ema0.1_alpha0.5_wu_ub_lang_dro_ted8"
 
 for prefix in $dir; do
     for pp in related_o2m related_m2o diverse_o2m diverse_m2o; do
@@ -8,9 +8,12 @@ for prefix in $dir; do
         if [ ! -d  $dd ]; then
             continue
         fi
-        scp $dd/test_* tir:/home/chuntinz/tir5/logs/${prefix}_${pp}/
-        if [ -f "${dd}/inner_log.txt" ]; then
-            scp $dd/inner_log.txt tir:/home/chuntinz/tir5/logs/${prefix}_${pp}/
-        fi
+        ssh tir "mkdir /home/chuntinz/tir5/logs/${prefix}_${pp}/"
+        scp $dd/run.sh tir:/home/chuntinz/tir5/logs/${prefix}_${pp}/
+        scp $dd/*log.txt tir:/home/chuntinz/tir5/logs/${prefix}_${pp}/
+        scp $dd/test_*log tir:/home/chuntinz/tir5/logs/${prefix}_${pp}/
+#        if [ -f "${dd}/inner_log.txt" ]; then
+#            scp $dd/inner_log.txt tir:/home/chuntinz/tir5/logs/${prefix}_${pp}/
+#        fi
     done
 done
