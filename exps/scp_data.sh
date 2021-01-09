b80a9c443a1dfc6861ef7bff37667d23bee74a32
Chunting Zhou <chuntinz@fb.com>
2021-01-08 18:41:51 -0800

    add scripts for opus

--- exps/scp_data.sh
+++ exps/scp_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-scp tir:/home/chuntinz/tir5/data/mnmt_data/ted/ted8_diverse/data-bin/*baselines /private/home/ghazvini/chunting/data/mnmt_data/ted/ted8_diverse/data-bin/
-scp tir:/home/chuntinz/tir5/data/mnmt_data/ted/ted8_related/data-bin/*baselines /private/home/ghazvini/chunting/data/mnmt_data/ted/ted8_related/data-bin/
+scp -r tir:/home/chuntinz/tir5/data/mnmt_data/opus10 /private/home/ghazvini/chunting/data/mnmt_data/
+#scp tir:/home/chuntinz/tir5/data/mnmt_data/ted/ted8_diverse/data-bin/*baselines /private/home/ghazvini/chunting/data/mnmt_data/ted/ted8_diverse/data-bin/
+#scp tir:/home/chuntinz/tir5/data/mnmt_data/ted/ted8_related/data-bin/*baselines /private/home/ghazvini/chunting/data/mnmt_data/ted/ted8_related/data-bin/
