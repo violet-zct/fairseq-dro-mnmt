6956d59f06c6c1f71d0a5d40b89c6e4ac42ecdd2
Daniel Levy <daniel.levy0@gmail.com>
2021-03-06 19:48:07 -0800

    new projection

--- debug_chi_square.py
+++ debug_chi_square.py
@@ -93,10 +93,10 @@ def project_to_cs_ball(v, rho, p_train):
     n = len(v)
 
     def cs_div(p):
-        return 0.5 * np.sum((p - p_train)**2)
+        return 0.5 * (np.square(p / p_train - 1) * p_train).sum()
 
     # first, check if a simplex projection is within the chi-square ball
-    target_simplex = lambda eta: np.sum(np.maximum(v - eta, 0)) - 1.0
+    target_simplex = lambda eta: np.sum(p_train * np.maximum(v - eta, 0)) - 1.0
     eta_min_simplex = v.min() - 1 / n
     eta_max_simplex = v.max()
     eta_simplex = optimize.brentq(
@@ -107,7 +107,7 @@ def project_to_cs_ball(v, rho, p_train):
 
     # second, compute a chi-square best response
     def target_cs(eta, return_p=False):
-        p = np.maximum(v - eta, 0)
+        p = p_train * np.maximum(v - eta, 0)
         if p.sum() == 0.0:
             p[np.argmax(v)] = 1.0
         else:
