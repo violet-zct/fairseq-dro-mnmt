727308d24a08f922565de525d546d4a8b814de51
Chunting Zhou <chuntinz@fb.com>
2021-03-11 15:01:24 -0800

    fix

--- utils/compute_ptrain_by_tokens.py
+++ utils/compute_ptrain_by_tokens.py
@@ -1,5 +1,8 @@
 import os
 
+split = "train"
+print(split)
+
 
 def opt(root, langs, dataset):
     data_dir = os.path.join(root)
@@ -7,15 +10,27 @@ def opt(root, langs, dataset):
 
     eng_toks = []
     lan_toks = []
+
+    sents = []
     for lang in langs:
-        flang = os.path.join(data_dir, "{}_en".format(lang), "spm.train.{}".format(lang))
-        fengl = os.path.join(data_dir, "{}_en".format(lang), "spm.train.en")
-        neng_toks = len(open(fengl, encoding="utf-8").read().split())
-        nlan_toks = len(open(flang, encoding="utf-8").read().split())
-        eng_toks.append(neng_toks)
-        lan_toks.append(nlan_toks)
+        flang = os.path.join(data_dir, "{}_en".format(lang), "spm.{}.{}".format(split, lang))
+        fengl = os.path.join(data_dir, "{}_en".format(lang), "spm.{}.en".format(split))
+        temp_num_eng_toks, temp_num_lang_toks = 0, 0
+        temp_num_sents = 0
+        with open(fengl, encoding="utf-8") as fen, open(flang, encoding="utf-8") as flan:
+            for leng, llang in zip(fen, flan):
+                len_eng, len_lang = leng.strip().split(), len_lang.strip().split()
+                if len(len_eng) < 512 and len(len_lang) < 512:
+                    temp_num_eng_toks += len_eng
+                    temp_num_lang_toks += len_lang
+                    temp_num_sents += 1
+        eng_toks.append(temp_num_eng_toks)
+        lan_toks.append(temp_num_lang_toks)
+        sents.append(temp_num_sents)
 
     print(dataset)
+    print("ptrain by sents")
+    print({lang: sents[ii] / sum(sents) for ii, lang in enumerate(langs)})
     print("m2o")
     print({lang:eng_toks[ii]/sum(eng_toks) for ii, lang in enumerate(langs)})
     print("o2m")
