d8e1330bed5ae5a27fb1288d9d20954042ddcc5e
Chunting Zhou <chuntinz@fb.com>
2021-04-01 17:23:52 -0700

    WIP

--- utils/process_scripts/clean.sh
+++ utils/process_scripts/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPTS=mosesdecoder/scripts
+SCRIPTS=/jet/home/chuntinz/work/data/wmt/mosesdecoder/scripts
 NORM_PUNC=$SCRIPTS/tokenizer/normalize-punctuation.perl
 REM_NON_PRINT_CHAR=$SCRIPTS/tokenizer/remove-non-printing-char.perl
 
