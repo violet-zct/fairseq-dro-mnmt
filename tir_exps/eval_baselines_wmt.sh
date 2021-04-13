5f7a52cfb33f2730ad7bd621b5b34d0c3bf763c4
violet-zct <chuntinz@cs.cmu.edu>
2021-04-12 23:56:39 -0400

    update

--- tir_exps/eval_baselines_wmt.sh
+++ tir_exps/eval_baselines_wmt.sh
@@ -7,11 +7,11 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=10g
 #SBATCH --time=0
-#SBATCH --array=0-3
-#SBATCH --exclude=compute-0-31,compute-1-7
+#SBATCH --array=0-5
+#SBATCH --exclude=compute-0-36,compute-0-31,compute-1-7
 
 source activate hal
-models=(57_erm_temp_1_wmt4_de_m2o 57_erm_temp_100_wmt4_de_m2o 57_erm_temp_1_wmt4_de_o2m 57_erm_temp_100_wmt4_de_o2m)
+models=(57_erm_temp_1_wmt4_de_m2o 57_erm_temp_100_wmt4_de_m2o 57_erm_temp_5_wmt4_de_m2o 57_erm_temp_1_wmt4_de_o2m 57_erm_temp_5_wmt4_de_o2m 57_erm_temp_100_wmt4_de_o2m)
 model=${models[${SLURM_ARRAY_TASK_ID}]}
 
 SAVE=/home/chuntinz/tir5/logs/${model}
@@ -46,11 +46,11 @@ arch=transformer_wmt_en_de
 #	  --max-tokens 4096 --log-interval 100 --log-format simple | tee ${SAVE}/valid_log.txt
 
 python -u fairseq_cli/compute_baseline_loss.py ${DATA}\
-	  --task translation_multi_simple_epoch \
+	  --task translation_multi_simple_epoch --restore-file checkpoint_best.pt \
 	  --arch ${arch} --valid-subset train --skip-invalid-size-inputs-valid-test \
 	  --sampling-method "temperature" --sampling-temperature 1 \
 	  --encoder-langtok ${etok} --group-level ${glevel} \
-	  --max-update 300000 --layernorm-embedding \
+	  --max-update 100 --layernorm-embedding \
     --lang-pairs ${lang_pairs} \
     --lang-dict ${DATA}/langs.list \
 	  --no-epoch-checkpoints \
