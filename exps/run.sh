4eeca1ad7560d043b6764efc7a7ef343bca1ef06
Chunting Zhou <chuntinz@fb.com>
2021-04-17 23:02:19 -0700

    update

--- exps/run.sh
+++ exps/run.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
-sbatch exps/55_wmt_erm_baseline.sh
-sbatch exps/55_wmt_erm_temp1_baseline.sh
-sbatch exps/56_chi_square_resample_wmt4.sh
-sbatch exps/57_wmt_de_erm_baseline.sh
-sbatch exps/57_wmt_de_erm_temp1_baseline.sh
-sbatch exps/58_chi_square_resample_wmt4_de.sh
+sbatch exps/64_td_select_baselines_chi_square_wmt.sh
+sbatch exps/65_td_c0.0_select_baselines_chi_square_wmt.sh
+sbatch exps/66_td_c0.05_select_no_baselines_chi_square_wmt.sh
+sbatch exps/67_td_select_rho_0.1_baselines_chi_square_wmt.sh
