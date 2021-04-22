b6640d90388ba9018d0c31a795e75f929504dd3e
Chunting Zhou <chuntinz@fb.com>
2021-04-21 22:27:43 -0700

    fix

--- exps/run.sh
+++ exps/run.sh
@@ -6,7 +6,7 @@ rm -rf /checkpoint/xianl/space/dro_mnt/66*
 rm -rf /checkpoint/xianl/space/dro_mnt/67*
 
 # keeps updating train dynamics; fix bugs; try sample based selection
-sbatch exps/69_warmup1_burnout20_td_c0.0_select_baselines_chi_square_wmt.sh
-sbatch exps/70_warmup20_burnout20_td_c0.0_select_baselines_chi_square_wmt.sh
-sbatch exps/71_sample_warmup1_burnout20_td_c0.0_select_baselines_chi_square_wmt.sh
-sbatch exps/72_sample_warmup20_burnout20_td_c0.0_select_baselines_chi_square_wmt.sh
+sbatch exps/69_warmup1_burnout20_td_c0.05_select_baselines_chi_square_wmt.sh
+sbatch exps/70_warmup20_burnout20_td_c0.05_select_baselines_chi_square_wmt.sh
+sbatch exps/71_sample_warmup1_burnout20_td_c0.05_select_baselines_chi_square_wmt.sh
+sbatch exps/72_sample_warmup20_burnout20_td_c0.05_select_baselines_chi_square_wmt.sh
