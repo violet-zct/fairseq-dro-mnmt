74beaaf6010f04e721a163f6dd7eb08107d05854
Chunting Zhou <chuntinz@fb.com>
2021-04-23 14:07:57 -0700

    fix

--- exps/run.sh
+++ exps/run.sh
@@ -5,7 +5,7 @@
 #rm -rf /checkpoint/xianl/space/dro_mnt/66*
 #rm -rf /checkpoint/xianl/space/dro_mnt/67*
 
-scancel 40411227
+
 rm -rf /checkpoint/xianl/space/dro_mnt/73*
 rm -rf /checkpoint/xianl/space/dro_mnt/74*
 rm -rf /checkpoint/xianl/space/dro_mnt/75*
@@ -25,3 +25,11 @@ rm -rf /checkpoint/xianl/space/dro_mnt/75*
 #sbatch exps/73_cl_wmt_de_erm_temp5_baseline.sh
 #sbatch exps/74_cl_hardness_sum_warmup5_baselines_chi_square_wmt.sh
 #sbatch exps/75_cl_hardness_min_prob_warmup5_baselines_chi_square_wmt.sh
+
+scancel 40411227
+scancel 40367139
+sbatch exps/69_warmup1_burnout20_td_c0.05_select_baselines_chi_square_wmt.sh
+sbatch exps/76_0.5_baselines_chi_square_resample_wmt4_de_o2m.sh
+
+scp /checkpoint/xianl/space/dro_mnt/68_erm_train_dynamics_wmt14_ende_ende/checkpoint*pt tir:/home/chuntinz/tir5/logs/68_erm_train_dynamics_wmt14_ende_ende/
+scp /checkpoint/xianl/space/dro_mnt/68_erm_train_dynamics_wmt14_ende_deen/checkpoint*pt tir:/home/chuntinz/tir5/logs/68_erm_train_dynamics_wmt14_ende_deen/
--- exps/scp.sh
+++ exps/scp.sh
@@ -35,5 +35,7 @@
 
 scancel 40367139
 sbatch exps/69_warmup1_burnout20_td_c0.05_select_baselines_chi_square_wmt.sh
+sbatch exps/76_0.5_baselines_chi_square_resample_wmt4_de_o2m.sh
+
 scp /checkpoint/xianl/space/dro_mnt/68_erm_train_dynamics_wmt14_ende_ende/checkpoint*pt tir:/home/chuntinz/tir5/logs/68_erm_train_dynamics_wmt14_ende_ende/
 scp /checkpoint/xianl/space/dro_mnt/68_erm_train_dynamics_wmt14_ende_deen/checkpoint*pt tir:/home/chuntinz/tir5/logs/68_erm_train_dynamics_wmt14_ende_deen/
