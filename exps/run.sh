ff08a17b473b23a7667bf48a7ef72ac659857d33
violet-zct <chuntinz@cs.cmu.edu>
2021-05-21 20:42:58 -0700

    fix

--- exps/run.sh
+++ exps/run.sh
@@ -5,7 +5,7 @@ datapath=/private/home/ghazvini/chunting/data/mnmt_data
 root=/private/home/ghazvini/chunting/fairseq-dro-mnmt
 tirroot=/home/chuntinz/tir5/logs
 
-scp  tir:${tirroot}/57_erm_temp_100_wmt4_de_o2m/train_sents_o2m.loss ${datapath}/wmt4/data-bin-v2/
+#scp  tir:${tirroot}/57_erm_temp_100_wmt4_de_o2m/train_sents_o2m.loss ${datapath}/wmt4/data-bin-v2/
 
 sbatch exps/88_sent_baselined_chi_square_batch_dro_wmt4_de.sh
 sbatch exps/89_group_baselined_chi_square_batch_dro_wmt4_de.sh
